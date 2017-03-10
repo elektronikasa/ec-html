c4934971836239941051366076882cbf3fc673e0
Jimmy <hello@elektronika.co.za>
2017-03-10 11:59:23 +0200

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create(); // create a browser sync instance.
+
+
+gulp.task('browser-sync', function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', bs.reload);
+});
